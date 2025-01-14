2457c690595576ed4c711df2c264f7e9751fb2e5
ChiVi123 <nguyenhoangchivi@gmail.com>
2025-01-14 15:46:20 +0700

    move files (init project) to right folders

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import './index.css';
+import '~css/main.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
+import App from './view/app';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
